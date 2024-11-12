7d7bb77c333b46fd1d6a3813845ba9549e270677
Pedro Raulino <pedrohraulino@gmail.com>
2024-11-12 17:56:27 -0300

    atividade OO

--- main.js
+++ main.js
@@ -1,31 +1,107 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const nome = document.getElementById('nome');
-    const user = document.getElementById('user');
-    const img = document.getElementById('img');
-    const repositorios = document.getElementById('repositorios');
-    const seguidores = document.getElementById('seguidores');
-    const seguindo = document.getElementById('seguindo');
-    const link = document.getElementById('link');
+class Docente {
+    constructor(nome, matricula, salario) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.salario = salario;
+    }
 
-    const url = 'https://api.github.com/users/pedrohraulino';
+    getNome() {
+        return this.nome;
+    }
 
-    fetch(url)
-        .then(function (response) {
-            if (!response.ok) throw new Error('Erro na requisição');
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            nome.textContent = data.name;
-            user.textContent = data.login;
-            img.src = data.avatar_url;
-            repositorios.textContent = data.public_repos;
-            seguidores.textContent = data.followers;
-            seguindo.textContent = data.following;
-            link.href = `https://github.com/${data.login}`;
+    setNome(nome) {
+        this.nome = nome;
+    }
 
-        })
-        .catch(function (error) {
-            console.error('Erro:', error);
-        });
-});
+    getMatricula() {
+        return this.matricula;
+    }
+
+    setMatricula(matricula) {
+        this.matricula = matricula;
+    }
+
+    getSalario() {
+        return this.salario;
+    }
+
+    setSalario(salario) {
+        this.salario = salario;
+    }
+
+    updateSalario() {
+        this.salario *= 1.2; // Atualiza o próprio salário
+    }
+}
+
+class Professor extends Docente {
+    constructor(nome, matricula, salario, materia) {
+        super(nome, matricula, salario);
+        this.materia = materia;
+    }
+
+    getMateria() {
+        return this.materia;
+    }
+
+    setMateria(materia) {
+        this.materia = materia;
+    }
+
+    updateSalario() {
+        this.salario *= 1.3;
+    }
+}
+
+class ProfessorSubstituto extends Professor {
+    constructor(nome, matricula, salario, materia, professorTitular) {
+        super(nome, matricula, salario, materia);
+        this.professorTitular = professorTitular;
+    }
+
+    getProfessorTitular() {
+        return this.professorTitular;
+    }
+
+    setProfessorTitular(professorTitular) {
+        this.professorTitular = professorTitular;
+    }
+}
+
+class Tecnico extends Docente {
+    constructor(nome, matricula, salario, setor) {
+        super(nome, matricula, salario);
+        this.setor = setor;
+    }
+
+    getSetor() {
+        return this.setor;
+    }
+
+    setSetor(setor) {
+        this.setor = setor;
+    }
+
+}
+
+
+const professor = new Professor("Ana Souza", "P001", 4000, "Matemática");
+console.log("Nome do Professor:", professor.getNome()); 
+console.log("Matéria do Professor:", professor.getMateria());
+console.log("Salário inicial do Professor:", professor.getSalario());
+professor.updateSalario();
+console.log("Salário atualizado do Professor:", professor.getSalario()); 
+
+const professorSubstituto = new ProfessorSubstituto("João Pedro", "PS001", 2500, professor.materia, professor);
+console.log("\nNome do Professor Substituto:", professorSubstituto.getNome());
+console.log("Matéria do Professor Substituto:", professorSubstituto.getMateria()); 
+console.log("Salário inicial do Professor Substituto:", professorSubstituto.getSalario());
+console.log("Nome do Professor Titular:", professorSubstituto.getProfessorTitular().getNome());
+
+const tecnico = new Tecnico("Mariana Costa", "T001", 3500, "Administrativo");
+console.log("\nNome do Técnico:", tecnico.getNome());
+console.log("Setor do Técnico:", tecnico.getSetor()); 
+console.log("Salário inicial do Técnico:", tecnico.getSalario()); 
+tecnico.updateSalario();
+console.log("Salário atualizado do Técnico:", tecnico.getSalario()); 
+
