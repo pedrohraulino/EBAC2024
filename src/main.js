c96e7e2f0de97c06322e57305d27cfd81757779e
Pedro Raulino <pedrohraulino@gmail.com>
2024-11-16 19:07:36 -0300

    atividade react

--- src/main.js
+++
@@ -1,4 +0,0 @@
-var multiplication = function (num1, num2) { return num1 * num2; };
-console.log(multiplication(2, 5));
-var sayHellow = function (name) { return "".concat(name, " say hellow"); };
-console.log(sayHellow("pedro"));
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-const multiplication = (num1: number, num2: number): number => num1 * num2;
-
-console.log(multiplication(2,5))
-
-const sayHellow = (name:string):string => `${name} say hellow`;
-
-console.log(sayHellow("Pedro"))
